1909f97e00e99e480d29c38b18d5a72dcf4d792e
ItsRhiana <ringosao0504@gmail.com>
2026-08-28 19:32:36 +0700

    adds utils

---
+++ astro-site/src/data/styles/index.ts
@@ -0,0 +1,151 @@
+import lodestarSeaRover from "./lodestar-sea-rover";
+
+const styles = {
+  "red-gloves-abyssal-judgment": {
+    id: "red-gloves-abyssal-judgment",
+    characterName: "Red Gloves",
+    styleName: "Abyssal Judgment",
+    rarity: 6,
+    desire: "lust",
+  },
+
+  "fool-card-table-seer": {
+    id: "fool-card-table-seer",
+    characterName: "Fool",
+    styleName: "Card Table Seer",
+    rarity: 6,
+    desire: "envy",
+  },
+
+  "wolf-patrol-guard": {
+    id: "wolf-patrol-guard",
+    characterName: "Wolf",
+    styleName: "Patrol Guard",
+    rarity: 6,
+    desire: "greed",
+  },
+
+  "general-thunder-commander": {
+    id: "general-thunder-commander",
+    characterName: "General",
+    styleName: "Thunder Commander",
+    rarity: 6,
+    desire: "sloth",
+  },
+
+  "windward-money-loving-gentleman": {
+    id: "windward-money-loving-gentleman",
+    characterName: "Windward",
+    styleName: "Money-Loving Gentleman",
+    rarity: 6,
+    desire: "greed",
+  },
+
+  "tyrant-lord-of-terra": {
+    id: "tyrant-lord-of-terra",
+    characterName: "Tyrant",
+    styleName: "Lord of Terra",
+    rarity: 6,
+    desire: "pride",
+  },
+
+  "lodestar-sea-rover": lodestarSeaRover,
+
+  "ghost-vengeful-nursery-rhyme": {
+    id: "ghost-vengeful-nursery-rhyme",
+    characterName: "Ghost",
+    styleName: "Vengeful Nursery Rhyme",
+    rarity: 6,
+    desire: "envy",
+  },
+
+  "ng-dream-producer": {
+    id: "ng-dream-producer",
+    characterName: "NG",
+    styleName: "Dream Producer",
+    rarity: 6,
+    desire: "greed",
+  },
+
+  "rainmaker-world-cleansing-rain": {
+    id: "rainmaker-world-cleansing-rain",
+    characterName: "Rainmaker",
+    styleName: "World-Cleansing Rain",
+    rarity: 6,
+    desire: "gluttony",
+  },
+
+  "red-gloves-ace-lawyer": {
+    id: "red-gloves-ace-lawyer",
+    characterName: "Red Gloves",
+    styleName: "Ace Lawyer",
+    rarity: 5,
+    desire: "sloth",
+  },
+
+  "chef-veteran-butcher": {
+    id: "chef-veteran-butcher",
+    characterName: "Chef",
+    styleName: "Veteran Butcher",
+    rarity: 5,
+    desire: "greed",
+  },
+
+  "nobody-seaside-holiday": {
+    id: "nobody-seaside-holiday",
+    characterName: "Nobody",
+    styleName: "Seaside Holiday",
+    rarity: 5,
+    desire: "pride",
+  },
+
+  "headmistress-scorching-sands-knight": {
+    id: "headmistress-scorching-sands-knight",
+    characterName: "Headmistress",
+    styleName: "Scorching Sands Knight",
+    rarity: 6,
+    desire: "gluttony",
+  },
+
+  "gift-player-in-the-play": {
+    id: "gift-player-in-the-play",
+    characterName: "Gift",
+    styleName: "Player in the Play",
+    rarity: 5,
+    desire: "gluttony",
+  },
+
+  "laksa-spice-merchant": {
+    id: "laksa-spice-merchant",
+    characterName: "Laksa",
+    styleName: "Spice Merchant",
+    rarity: 5,
+    desire: "lust",
+  },
+
+  "manipulator-technology-minister": {
+    id: "manipulator-technology-minister",
+    characterName: "Manipulator",
+    styleName: "Technology Minister",
+    rarity: 5,
+    desire: "greed",
+  },
+
+  "ghost-covert-investigator": {
+    id: "ghost-covert-investigator",
+    characterName: "Ghost",
+    styleName: "Covert Investigator",
+    rarity: 5,
+    desire: "envy",
+  },
+
+  "the-ref-silent-verdict": {
+    id: "the-ref-silent-verdict",
+    characterName: "The Ref",
+    styleName: "Silent Verdict",
+    rarity: 5,
+    desire: "sloth",
+  },
+} as const;
+
+export default styles;
---
+++ astro-site/src/data/tier-lists/infinite-stairway.ts
@@ -0,0 +1,133 @@
+const infiniteStairway = {
+  columns: [
+    {
+      id: 'vanguard',
+      name: 'Vanguard',
+      icon: 'vanguard',
+    },
+    {
+      id: 'st-dps',
+      name: 'ST DPS',
+      icon: 'annihilation',
+    },
+    {
+      id: 'aoe-dps',
+      name: 'AoE DPS',
+      icon: 'annihilation',
+    },
+    {
+      id: 'support',
+      name: 'Support',
+      icon: 'special-attack',
+    },
+    {
+      id: 'healer',
+      name: 'Healer',
+      icon: 'healer',
+    },
+  ],
+
+  tiers: [
+    {
+      rank: 'T0',
+
+      cells: {
+        vanguard: [
+          'red-gloves-abyssal-judgment',
+        ],
+
+        'st-dps': [
+          'fool-card-table-seer',
+        ],
+
+        'aoe-dps': [
+          'wolf-patrol-guard',
+        ],
+
+        support: [
+          'general-thunder-commander',
+        ],
+
+        healer: [
+          'windward-money-loving-gentleman',
+        ],
+      },
+    },
+
+    {
+      rank: 'T1',
+
+      cells: {
+        vanguard: [
+          'tyrant-lord-of-terra',
+        ],
+
+        'st-dps': [
+          'lodestar-sea-rover',
+        ],
+
+        'aoe-dps': [
+          'ghost-vengeful-nursery-rhyme',
+        ],
+
+        support: [
+          'ng-dream-producer',
+        ],
+
+        healer: [
+          'rainmaker-world-cleansing-rain',
+        ],
+      },
+    },
+
+    {
+      rank: 'T2',
+
+      cells: {
+        vanguard: [
+          'red-gloves-ace-lawyer',
+        ],
+
+        'st-dps': [
+          'chef-veteran-butcher',
+        ],
+
+        'aoe-dps': [
+          'nobody-seaside-holiday',
+        ],
+
+        support: [
+          'headmistress-scorching-sands-knight',
+          'gift-player-in-the-play',
+        ],
+
+        healer: [
+          'laksa-spice-merchant',
+        ],
+      },
+    },
+
+    {
+      rank: 'T3',
+
+      cells: {
+        vanguard: [],
+
+        'st-dps': [],
+
+        'aoe-dps': [
+          'manipulator-technology-minister',
+        ],
+
+        support: [
+          'ghost-covert-investigator',
+          'the-ref-silent-verdict',
+        ],
+
+        healer: [],
+      },
+    },
+  ],
+} as const;
+
+export default infiniteStairway;
